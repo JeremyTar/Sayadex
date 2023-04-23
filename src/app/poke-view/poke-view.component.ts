f70c82d2408fbc3ffcc6393b0b9f744f26bbd527
JeremyTar <jeremy.tardini@accenture.com>
2023-04-23 20:32:07 +0200

    Add tables types

--- src/app/poke-view/poke-view.component.ts
+++ src/app/poke-view/poke-view.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PokemonClass, Abilitie, Type, PokemonType } from "../pokeClass"
+import { PokemonClass, Abilitie, Type, PokemonType, Sensibility, Resistance } from "../pokeClass"
 
 import { faLifeRing, faStar } from '@fortawesome/free-regular-svg-icons'
 import { faWeight, faArrowsUpDownLeftRight } from "@fortawesome/free-solid-svg-icons"
@@ -13,6 +13,7 @@ import { faWeight, faArrowsUpDownLeftRight } from "@fortawesome/free-solid-svg-i
 export class PokeViewComponent {
   constructor(private route: ActivatedRoute) { }
   pokemonInfo!: PokemonClass;
+  pokemonSensibility!: Sensibility[];
   mainImg!: string;
   shinyImg!: string;
   id!: number;
@@ -29,10 +30,107 @@ export class PokeViewComponent {
     this.route.queryParams.subscribe(params => {
       this.id = params['id']
     })
+
+    this.pokemonSensibility = [
+      {
+        name: "bug",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "dark",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "dragon",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "electric",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "fairy",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "fighting",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "fire",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "flying",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "ghost",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "grass",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "ground",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "ice",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "normal",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "poison",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "psychic",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "rock",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "steel",
+        indice: 0,
+        elements: []
+      },
+      {
+        name: "water",
+        indice: 0,
+        elements: []
+      }
+    ]
+
     await this.getPokemonInfo(this.id)
     await this.getType()
     await this.calculateAdvantageTypes(this.pokemonInfo)
-    
+    console.log(this.pokemonSensibility)
+    this.showTableTypes(this.pokemonSensibility)
+
+
   }
   showShinyButton() {
     if (this.showShiny == false) {
@@ -92,17 +190,79 @@ export class PokeViewComponent {
         this.pokemonInfo = pokemon
         this.mainImg = data.sprites.other['official-artwork'].front_default
         this.shinyImg = data.sprites.other['official-artwork'].front_shiny
-    })
+      })
   }
   async calculateAdvantageTypes(pokemon: PokemonClass) {
-    console.log(pokemon.type)
-    const myTypes:string[] = pokemon.type
-    for(const i in myTypes) {
+    const myTypes: string[] = pokemon.type
+    console.log(myTypes)
+    for (const i in myTypes) {
       await fetch(`https://pokeapi.co/api/v2/type/${myTypes[i]}`)
         .then(response => response.json())
         .then(data => {
-          console.log(data)
+          this.calculateResistance(data.damage_relations.half_damage_from, myTypes[i])
+          this.calculateWeakness(data.damage_relations.double_damage_from, myTypes[i])
+
         })
     }
+  }
+  calculateResistance(resistance: Resistance[], myType: string) {
+    resistance.forEach((element: Resistance) => {
+      let name = element.name
+      this.pokemonSensibility.forEach((el) => {
+        if (el.name == name) {
+          el.indice += 1
+          el.elements.push(myType)
+        }
+      })
+    });
+  }
+  calculateWeakness(weakness: any, myType: string) {
+    weakness.forEach((element: Resistance) => {
+      let name = element.name
+      this.pokemonSensibility.forEach((el) => {
+        if (el.name == name) {
+          el.indice -= 1
+          el.elements.push(myType)
+        }
+      })
+    });
+  }
+  showTableTypes(allElements: Sensibility[]) {
+    allElements.forEach((el) => {
+      let td = document.querySelector(`#${el.name}`)
+      if (td) {
+        switch (el.indice) {
+          case 1:
+            td.setAttribute("class", "resistance")
+            console.log("A")
+            break
+          case 2:
+            td.setAttribute("class", "doubleResistance")
+            console.log("B", td)
+            break
+          case -1:
+            td.setAttribute("class", "faiblesse")
+            console.log("C")
+            break
+          case -2:
+            td.setAttribute("class", "doublefaiblesse")
+            console.log("D")
+            break
+          default:
+            break
+        }
+      }
+      if (el.indice != 0) {
+        let td = document.querySelector(`#${el.name}`)
+        el.elements.forEach((name) => {
+          const img = document.createElement('img')
+          img.src = `../../assets/types/icone_size/${name}.png`
+          img.alt = `${name} type`
+          img.style.height = "20px"
+          img.style.width = "20px"
+          td?.append(img)
+        })
+      }
+    })
   }
 }
--- src/app/pokeClass.ts
+++ src/app/pokeClass.ts
@@ -16,6 +16,109 @@ export class PokemonClass {
     }
 }
 
+export interface Sensibility {
+  indice: number,
+  elements: string[]
+  name: string
+}
+
+export class SensibiltyClass {
+  bug: Sensibility = {
+    name: "bug",
+    indice: 0,
+    elements: []
+  };
+  dark: Sensibility = {
+    name: "dark",  
+    indice: 0,
+      elements: []
+  };
+  dragon: Sensibility = {
+    name: "dragon",
+    indice: 0,
+    elements: []
+  };
+  electric: Sensibility = {
+    name: "electric",
+    indice: 0,
+    elements: []
+  };
+  fairy: Sensibility = {
+    name: "fairy",
+    indice: 0,
+    elements: []
+  };
+  fighting: Sensibility = {
+    name: "fighting",
+    indice: 0,
+    elements: []
+  };
+  fire: Sensibility = {
+    name: "fire",
+    indice: 0,
+    elements: []
+  };
+  flying: Sensibility = {
+    name: "flying",
+    indice: 0,
+    elements: []
+  };
+  ghost: Sensibility = {
+    name: "ghost",
+    indice: 0,
+    elements: []
+  };
+  grass: Sensibility = {
+    name: "grass",
+    indice: 0,
+    elements: []
+  };
+  ground: Sensibility = {
+    name: "ground",
+    indice: 0,
+    elements: []
+  };
+  ice: Sensibility = {
+    name: "ice",
+    indice: 0,
+    elements: []
+  };
+  normal: Sensibility = {
+    name: "normal",
+    indice: 0,
+    elements: []
+  };
+  poison: Sensibility = {
+    name: "poison",
+    indice: 0,
+    elements: []
+  };
+  psychic: Sensibility = {
+    name: "psychic",
+    indice: 0,
+    elements: []
+  };
+  rock: Sensibility = {
+    name: "rock",
+    indice: 0,
+    elements: []
+  };
+  steel: Sensibility = {
+    name: "steel",
+    indice: 0,
+    elements: []
+  };
+  water: Sensibility = {
+    name: "water",
+    indice: 0,
+    elements: []
+  };
+}
+
+export interface Resistance {
+  name: string;
+}
+
 export interface PokemonType {
     slot: number,
     type: {
